eea7f686a07b8e35761767976f404d5412d7e408
Piyawanntkc <76213330+Piyawanntkc@users.noreply.github.com>
2022-05-20 00:07:54 +0700

    calculator

---
+++ myCalculator/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    colors :{
+      calBg : '#1a1a1a',
+      white : '#FFFFFF',
+      topCal : '#151515',
+      button : '#ff3e37',
+      btn: '#F6EAE3'
+    },
+  },
+  plugins: [],
+}
